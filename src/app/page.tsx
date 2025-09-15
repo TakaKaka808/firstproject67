376b68c4848e4ba3dfdbca4e3a593fdec64211c3
TakaKaka808 <bigbotobanger@proton.me>
2025-09-15 11:01:01 -1000

    PUshing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,22 @@
 import Link from "next/link";
+const mockUrls = ["https://li38l3utb2.ufs.sh/f/0J5Vzlz7TCSgnXkRUngQHfDFzkmhpUjLEsM5I0u7GogC3BaP","https://li38l3utb2.ufs.sh/f/0J5Vzlz7TCSglksOmmUrFI43o70nybmpaGcs1w2Tt96kqRPx","https://li38l3utb2.ufs.sh/f/0J5Vzlz7TCSgEPRbAMIUtsb3l8710fwumWKD2Zrdzhn6iage"]
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 
 export default function HomePage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-				<h1 className="font-extrabold text-5xl text-white tracking-tight sm:text-[5rem]">
-					Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-				</h1>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/usage/first-steps"
-						target="_blank"
-					>
-						<h3 className="font-bold text-2xl">First Steps →</h3>
-						<div className="text-lg">
-							Just the basics - Everything you need to know to set up your
-							database and authentication.
-						</div>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/introduction"
-						target="_blank"
-					>
-						<h3 className="font-bold text-2xl">Documentation →</h3>
-						<div className="text-lg">
-							Learn more about Create T3 App, the libraries it uses, and how to
-							deploy it.
-						</div>
-					</Link>
-				</div>
-			</div>
+			  <div className="flex flex-wrap gap-4">{
+      mockImages.map((image) => (
+        <div key ={image.id} className="w-48">
+          <img src={image.url} alt="image" />
+        </div>
+      ))
+    }
+    </div>
 		</main>
 	);
 }
